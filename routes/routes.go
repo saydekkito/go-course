adbe428c6c68fbfc1f0b963f8eb662de6a7bcd04
saydekkito <alexey.isaev.qq@gmail.com>
2025-11-07 07:18:16 +0800

    feat(routes): добавлен путь для авторизации и защита путей

--- routes/routes.go
+++ routes/routes.go
@@ -1,18 +1,24 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/saydekkito/go-course/controllers"
+	"github.com/saydekkito/go-course/controllers/bird_species"
+	"github.com/saydekkito/go-course/middleware"
 )
 
 func SetupRouter() *mux.Router {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/bird_species", controllers.GetAllBirdSpecies).Methods("GET")
-	r.HandleFunc("/bird_species/{id}", controllers.GetBirdSpecies).Methods("GET")
-	r.HandleFunc("/bird_species", controllers.CreateBirdSpecies).Methods("POST")
-	r.HandleFunc("/bird_species/{id}", controllers.UpdateBirdSpecies).Methods("PUT")
-	r.HandleFunc("/bird_species/{id}", controllers.DeleteBirdSpecies).Methods("DELETE")
+	r.HandleFunc("/login", controllers.Login).Methods("POST")
+	r.HandleFunc("/bird_species", bird_species.GetAllBirdSpecies).Methods("GET")
+	r.HandleFunc("/bird_species/{id}", bird_species.GetBirdSpecies).Methods("GET")
+
+	r.HandleFunc("/bird_species", middleware.JWTAuth(http.HandlerFunc(bird_species.CreateBirdSpecies))).Methods("POST")
+	r.HandleFunc("/bird_species/{id}", middleware.JWTAuth(http.HandlerFunc(bird_species.UpdateBirdSpecies))).Methods("PUT")
+	r.HandleFunc("/bird_species/{id}", middleware.JWTAuth(http.HandlerFunc(bird_species.DeleteBirdSpecies))).Methods("DELETE")
 
 	return r
 }
